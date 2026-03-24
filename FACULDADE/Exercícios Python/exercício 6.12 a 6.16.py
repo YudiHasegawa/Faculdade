bd63dabc689800c1618503951141468143e3603e
YudiHasegawa <yudireis1970@gmail.com>
2026-03-24 13:44:57 -0300

    asdasd

--- FACULDADE/Exercícios Python/exercício 6.12 a 6.16.py
+++ FACULDADE/Exercícios Python/exercício 6.12 a 6.16.py
@@ -1,17 +1,23 @@
-escolha = float(input("Digite o exercício desejado entre 6.12 6.13 : "))
+escolha = float(input("Digite o exercício desejado entre 6.12 6.13 6.16 : "))
 if escolha == 6.12:
     print("Exercício 6.12")
-    L = [1, 7, 2, 4]
-    máximo = L[0]
-    mínimo = L[0]
-    for x in L:
-        if x > máximo:
-            máximo = x
-    print(f"O maior número é {máximo}")
-    for y in L:
-        if y > máximo:
-            mínimo = y
-    print(f"O menor número é {mínimo}")
+    #Faça um programa que mostre o menor elemento de uma lista
+    L = []
+    while True:
+        elemento = int(input("Digite um número inteiro para adicionar na lista ou apertar 0 para sair: "))
+        L.append(elemento)
+        if elemento == 0:
+            break
+        máximo = L[0]
+        mínimo = L[0]
+        for x in L:
+            if x > máximo:
+                máximo = x
+        print(f"O maior número é {máximo}")
+        for y in L:
+            if y > máximo:
+                mínimo = y
+        print(f"O menor número é {mínimo}")
 if escolha == 6.13:
     print("Exercício 6.13")
     L = [-10, -8, 0, 1 , 2, 5, -2, -4]
@@ -30,3 +36,22 @@ if escolha == 6.13:
         soma += z
     média = soma / len(L)
     print(f"A média dos números é {média}")
+if escolha == 6.16:
+    print("Exercício 6.16")
+    L = [7, 4, 3, 12, 8]
+    fim = 5
+    while fim > 1:
+        trocou = False
+        x = 0
+        while x < (fim - 1):
+            if L[x] < L[x + 1]:
+                trocou = True
+                temp = L[x]
+                L[x] = L[x + 1]
+                L[x + 1] = temp
+            x += 1
+        if not trocou:
+            break
+        fim -= 1
+    for e in L:
+        print(e)
