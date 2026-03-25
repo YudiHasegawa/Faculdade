1b50f7f92f8325ec36c1cfa0b81b057dac48ce82
YudiHasegawa <yudireis1970@gmail.com>
2026-03-25 15:03:29 -0300

    asdasd

--- FACULDADE/Exercícios Python/teste.py
+++ FACULDADE/Exercícios Python/teste.py
@@ -1,3 +1,14 @@
-L = [1, 2, 3]
-L.append("banana")
-print(L)
+lista = {}
+todos = 0
+while True:
+    nome = input("\nDigite o item que você comprou ou 0 para sair: ")
+    if nome == "0":
+        break
+    quantidade = int(input("Digite a quantidade que comprou: "))
+    custo = float(input("Digite o preço do individual do item: "))
+    valor_final = quantidade * custo
+    lista[f"{nome}"] = [quantidade, custo, valor_final]
+    todos += lista[f"{nome}"][1]
+print("\nAqui está a tabela de gastos (item, quantidade, custo invidivual e custo total\n")
+for e in lista:
+    print(lista[f"{e}"])
